5c393b5de76937343b0a8763c7ea0940a21de90b
Javier Rivera <ingjrz@gmail.com>
2025-01-07 12:49:11 -0600

    Checking in AppConnect

---
+++ AppConnect/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppConnect
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppConnect/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppConnect
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
