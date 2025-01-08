0489039320724dd3a724ce02114db354f2db192f
Javier Rivera <ingjrz@gmail.com>
2025-01-08 17:41:11 -0600

    Other controls

--- ShopApp/MainPage.xaml.cs
+++ ShopApp/MainPage.xaml.cs
@@ -8,15 +8,6 @@ namespace ShopApp
         {
             InitializeComponent();
 
-            var dbContext = new ShopDbContext();
-            Category.Text = dbContext.Categories.Count().ToString();
-            Client.Text = dbContext.Clients.Count().ToString();
-            Product.Text = dbContext.Products.Count().ToString();
-        }
-
-        private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
-        {
-            await DisplayAlert("Hi", "Yes. the grid was tapped", "Ok", "Cancel");
         }
     }
 
