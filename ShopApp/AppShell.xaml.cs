9faf360d2360645c9c6a89dd1419f403a2796353
Javier Rivera <ingjrz@gmail.com>
2025-01-08 21:08:32 -0600

    Element Search and navigation to details.

--- ShopApp/AppShell.xaml.cs
+++ ShopApp/AppShell.xaml.cs
@@ -6,5 +6,11 @@
         {
             InitializeComponent();
         }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var uri = new Uri("https://www.newgrounds.com");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
     }
 }
---
+++ ShopApp/Handlers/ProductSearchHandler.cs
@@ -0,0 +1,32 @@
+﻿using ShopApp.DataAccess;
+using ShopApp.Views;
+
+namespace ShopApp.Handlers;
+
+public class ProductSearchHandler : SearchHandler
+{
+    ShopDbContext dbContext;
+
+    public ProductSearchHandler()
+    {
+        dbContext = new ShopDbContext();
+    }
+
+    protected override void OnQueryChanged(string oldValue, string newValue)
+    {
+        if (string.IsNullOrEmpty(newValue))
+        {
+            ItemsSource = null;
+            return;
+        }
+
+        ItemsSource = dbContext.Products
+            .Where(p => p.Name.ToLowerInvariant()
+                .Contains(newValue.ToLowerInvariant()));
+    }
+
+    protected async override void OnItemSelected(object item)
+    {
+        await Shell.Current.GoToAsync($"{nameof(ProductDetailPage)}?id={((Product)item).Id}");
+    }
+}
