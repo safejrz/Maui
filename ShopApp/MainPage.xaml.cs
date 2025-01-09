78b83da0a864a09e82fba2e961c4faca9de143f1
Javier Rivera <ingjrz@gmail.com>
2025-01-08 19:12:53 -0600

    Diseño de App con Resumen de Entidades.

--- ShopApp/MainPage.xaml.cs
+++ ShopApp/MainPage.xaml.cs
@@ -1,21 +1,27 @@
 ﻿using Microsoft.Maui.Controls.Shapes;
+using ShopApp.DataAccess;
 
 namespace ShopApp
 {
     public partial class MainPage : ContentPage
     {
+        int count = 0;
+
         public MainPage()
         {
             InitializeComponent();
-
+            var dbContext = new ShopDbContext();
+            Category.Text = dbContext.Categories.Count().ToString();
+            Product.Text = dbContext.Products.Count().ToString();
+            Client.Text = dbContext.Clients.Count().ToString();
         }
 
-        private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
-        {
-            (sender as Rectangle)?.ScaleTo(2);
-            (sender as Rectangle)?.TranslateTo(200, 200);
+        //private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        //{
+        //    (sender as Rectangle)?.ScaleTo(2);
+        //    (sender as Rectangle)?.TranslateTo(200, 200);
 
-        }
+        //}
     }
 
 }
