1464b67e5eda48b5f9fa896a5595684b2c2bd7e9
Javier Rivera <ingjrz@gmail.com>
2025-01-10 20:42:44 -0600

    Color converter for price

---
+++ ShopApp/Converters/PriceForColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ShopApp.Converters;
+
+public class PriceForColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var precio = (decimal)value;
+        if (precio >= 0 && precio <= 100)
+        {
+            return Colors.Green;
+        }
+
+        return Colors.Red;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- ShopApp/Views/ProductsPage.xaml.cs
+++ ShopApp/Views/ProductsPage.xaml.cs
@@ -9,20 +9,7 @@ public partial class ProductsPage : ContentPage
 	{
 		InitializeComponent();
 
-		var dbContext = new ShopDbContext();
-
-		foreach (var product in dbContext.Products)
-		{
-            //container.Children.Add(new Label { Text = product.Name });
-
-			var button = new Button { Text = product.Name };
-			button.Clicked += async (s, e) =>
-			{
-				var uri = $"{nameof(ProductDetailPage)}?id={product.Id}";
-				await Shell.Current.GoToAsync(uri);
-			};
-
-            container.Children.Add(button);
-        }
+        var dbContext = new ShopDbContext();
+        Products.ItemsSource = dbContext.Products;
     }
 }
