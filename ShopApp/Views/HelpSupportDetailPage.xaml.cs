0ce6f1c027138cfb00e2d9e80a9fa00477a041e6
Javier Rivera <ingjrz@gmail.com>
2025-01-10 19:55:37 -0600

    Navigation with bindings

--- ShopApp/Views/HelpSupportDetailPage.xaml.cs
+++ ShopApp/Views/HelpSupportDetailPage.xaml.cs
@@ -1,9 +1,15 @@
+
 namespace ShopApp.Views;
 
-public partial class HelpSupportDetailPage : ContentPage
+public partial class HelpSupportDetailPage : ContentPage, IQueryAttributable
 {
 	public HelpSupportDetailPage()
 	{
 		InitializeComponent();
 	}
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        Title = $"Client: {query["id"]}";
+    }
 }
--- ShopApp/Views/HelpSupportPage.xaml.cs
+++ ShopApp/Views/HelpSupportPage.xaml.cs
@@ -1,20 +1,21 @@
 using ShopApp.DataAccess;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace ShopApp.Views;
 
 public partial class HelpSupportPage : ContentPage
 {
-	public HelpSupportPage()
-	{
-		InitializeComponent();
-	}
+    public HelpSupportPage()
+    {
+        InitializeComponent();
+    }
 
-	private void Button_Clicked(object sender, EventArgs e)
-	{
-		var dataObject = Resources["data"] as HelpSupportData;
-		dataObject.VisitasPendientes = 30;
-	}
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var dataObject = Resources["data"] as HelpSupportData;
+        dataObject.VisitasPendientes = 30;
+    }
 }
 
 public class HelpSupportData : BindingUtilityObject
@@ -23,29 +24,39 @@ public class HelpSupportData : BindingUtilityObject
     {
         var database = new ShopDbContext();
         Clients = new ObservableCollection<Client>(database.Clients);
+        PropertyChanged += HelpSupportData_PropertyChanged;
+    }
+
+    private async void HelpSupportData_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if(e.PropertyName == nameof(SelectedClient))
+        {
+            var uri =$"{nameof(HelpSupportDetailPage)}?id={SelectedClient.Id}";
+            await Shell.Current.GoToAsync(uri);
+        }
     }
 
     private int _visitasPendientes;
 
-	public int VisitasPendientes 
-	{ 
-		get
-		{
-			return _visitasPendientes;
-		}
-		set 
-		{
-			if (_visitasPendientes != value)
-			{
-				_visitasPendientes = value;
-				RaisePropertyChanged();
-			}
-		} 
-	}
+    public int VisitasPendientes
+    {
+        get
+        {
+            return _visitasPendientes;
+        }
+        set
+        {
+            if (_visitasPendientes != value)
+            {
+                _visitasPendientes = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
 
-	private ObservableCollection<Client> _client;
+    private ObservableCollection<Client> _client;
 
-	public ObservableCollection<Client> Clients
+    public ObservableCollection<Client> Clients
     {
         get
         {
@@ -60,4 +71,18 @@ public class HelpSupportData : BindingUtilityObject
             }
         }
     }
+
+    private Client _selectedClient;
+
+    public Client SelectedClient
+    {
+        get { return _selectedClient; }
+        set
+        {
+            if (_selectedClient != value)
+                _selectedClient = value;
+            RaisePropertyChanged();
+        }
+    }
+
 }
