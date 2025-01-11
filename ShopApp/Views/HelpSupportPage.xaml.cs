e9494a782004740cdecb3b7c8758bfa471634ffc
Javier Rivera <ingjrz@gmail.com>
2025-01-10 19:44:46 -0600

    Data collections.

--- ShopApp/Views/HelpSupportPage.xaml.cs
+++ ShopApp/Views/HelpSupportPage.xaml.cs
@@ -1,3 +1,6 @@
+using ShopApp.DataAccess;
+using System.Collections.ObjectModel;
+
 namespace ShopApp.Views;
 
 public partial class HelpSupportPage : ContentPage
@@ -7,16 +10,22 @@ public partial class HelpSupportPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
-    {
+	private void Button_Clicked(object sender, EventArgs e)
+	{
 		var dataObject = Resources["data"] as HelpSupportData;
 		dataObject.VisitasPendientes = 30;
-    }
+	}
 }
 
 public class HelpSupportData : BindingUtilityObject
 {
-	private int _visitasPendientes;
+    public HelpSupportData()
+    {
+        var database = new ShopDbContext();
+        Clients = new ObservableCollection<Client>(database.Clients);
+    }
+
+    private int _visitasPendientes;
 
 	public int VisitasPendientes 
 	{ 
@@ -33,4 +42,22 @@ public class HelpSupportData : BindingUtilityObject
 			}
 		} 
 	}
+
+	private ObservableCollection<Client> _client;
+
+	public ObservableCollection<Client> Clients
+    {
+        get
+        {
+            return _client;
+        }
+        set
+        {
+            if (_client != value)
+            {
+                _client = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
 }
