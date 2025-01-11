43b4342c0387b21a1a04660f8c251305d686f2c4
Javier Rivera <ingjrz@gmail.com>
2025-01-10 20:29:31 -0600

    Data templates and reuse of them in GUI

--- ShopApp/Views/HelpSupportDetailPage.xaml.cs
+++ ShopApp/Views/HelpSupportDetailPage.xaml.cs
@@ -1,15 +1,73 @@
 
+using ShopApp.DataAccess;
+using System.Collections.ObjectModel;
+
 namespace ShopApp.Views;
 
 public partial class HelpSupportDetailPage : ContentPage, IQueryAttributable
 {
-	public HelpSupportDetailPage()
-	{
-		InitializeComponent();
-	}
+    public HelpSupportDetailPage()
+    {
+        InitializeComponent();
+    }
 
     public void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         Title = $"Client: {query["id"]}";
     }
+}
+
+public class HelpSupportDetailData : BindingUtilityObject
+{
+    public HelpSupportDetailData()
+    {
+        var database = new ShopDbContext();
+        Products = new ObservableCollection<Product>(database.Products);
+    }
+
+    private ObservableCollection<Product> _products;
+
+    public ObservableCollection<Product> Products
+    {
+        get { return _products; }
+        set
+        {
+            if (_products != value)
+            {
+                _products = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+
+    private Product _selectedProduct;
+
+    public Product SelectedProduct
+    {
+        get { return _selectedProduct; }
+        set
+        {
+            if (_selectedProduct != value)
+            {
+                _selectedProduct = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+
+    private int _quantity;
+
+    public int Quantity
+    {
+        get { return _quantity; }
+        set
+        {
+            if (_quantity != value)
+            {
+                _quantity = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+
 }
